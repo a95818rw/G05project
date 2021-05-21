7f0c21b6e5e04d23b5409319115cc77a6668a392
Shen <gpdgpa@gmail.com>
2021-05-21 22:27:13 +0800

    change db

--- utils/database.js
+++ utils/database.js
@@ -7,12 +7,12 @@ const mysql = require('mysql2');
 //   database: 'map',
 // });
 
-// const pool = mysql.createPool({
-//   host: 'us-cdbr-east-03.cleardb.com',
-//   user: 'bcba83ee034efd',
-//   password: '4242b364',
-//   database: 'heroku_cbf44c9d77f07e2',
-// });
+const pool = mysql.createPool({
+  host: 'us-cdbr-east-03.cleardb.com',
+  user: 'bcba83ee034efd',
+  password: '4242b364',
+  database: 'heroku_cbf44c9d77f07e2',
+});
 
 
 // test
